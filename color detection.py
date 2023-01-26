a5675e009679585b63bdff47a22f5db78b376b74
flukechd <73994876+flukechd@users.noreply.github.com>
2023-01-26 10:30:27 +0700

    Add files via upload

--- color detection.py
+++ color detection.py
@@ -11,16 +11,10 @@ while True:
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Define the range of red colors to detect
-    # lower_red = (136, 87, 111)
-    # upper_red = (180, 255, 255)
-    # Define the range of Green colors to detect
-    lower_red = (25, 52, 72)
-    upper_red = (102, 255, 255)
-    # Define the range of blue colors to detect
-    # lower_red = (94, 80, 2)
-    # upper_red = (120, 255, 255)
+    lower = (136, 87, 111)
+    upper = (180, 255, 255)
     # Create a mask for the frame using the lower and upper bounds of the red color
-    mask = cv2.inRange(frame_hsv, lower_red, upper_red)
+    mask = cv2.inRange(frame_hsv, lower, upper)
 
     # Apply the mask to the frame
     result = cv2.bitwise_and(frame, frame, mask=mask)
